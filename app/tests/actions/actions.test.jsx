58878e8587775bc96be0ff4aacd2d41087c60536
Tim <tim.lisiecki@gmail.com>
2017-05-07 14:30:58 -0400

    fetch todos from firebase on app start

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -87,17 +87,24 @@ describe("Actions", () => {
 		expect(res).toEqual(action);
 	});
 
-	describe("Tests with Firebas todos", () => {
+	describe("Tests with Firebase todos", () => {
 		var testTodoRef;
 
 		beforeEach((done) => {
-			testTodoRef = firebaseRef.child("todos").push();
+			var todosRef = firebaseRef.child("todos");
 
-			testTodoRef.set({
-				text: "Something to do",
-				completed: false,
-				createdAt: 3575252
-			}).then(() => done());
+			todosRef.remove().then(() => {
+				testTodoRef = firebaseRef.child("todos").push();
+
+				return testTodoRef.set({
+					text: "Something to do",
+					completed: false,
+					createdAt: 3575252
+				});
+			})
+			.then(() => done())
+			.catch(done);
+
 		});
 
 		afterEach((done) => {
@@ -119,6 +126,21 @@ describe("Actions", () => {
 					completed: true
 				});
 				expect(mockActions[0].updates.completedAt).toExist();
+
+				done();
+			}, done);
+		});
+
+		it("should populate todos and dispatch ADD_TODOS", (done) => {
+			const store = createMockStore({});
+			const action = actions.startAddTodos();
+
+			store.dispatch(action).then(() => {
+				const mockActions = store.getActions();
+
+				expect(mockActions[0].type).toEqual("ADD_TODOS");
+				expect(mockActions[0].todos.length).toEqual(1);
+				expect(mockActions[0].todos[0].text).toEqual("Something to do");
 
 				done();
 			}, done);
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -7,52 +7,6 @@ describe("TodoAPI", () => {
 		expect(TodoAPI).toExist();
 	});
 
-	describe("setTodos", () => {
-		beforeEach(() => {
-			localStorage.removeItem("todos");
-		});
-
-		it("should set valid todos array", () => {
-			var todos = [{
-				id: 23,
-				text: "Test all files",
-				completed: false
-			}];
-			TodoAPI.setTodos(todos);
-
-			var actualTodos = JSON.parse(localStorage.getItem("todos"));
-
-			expect(actualTodos).toEqual(todos);
-		});
-
-		it("should not set invalid todos array", () => {
-			var badTodos = {a: "b"};
-			TodoAPI.setTodos(badTodos);
-
-			expect(localStorage.getItem("todos")).toBe(null);
-		});
-	});
-
-	describe("getTodos", () => {
-		it("should return empty array for bad localstorage data", () => {
-			var actualTodos = TodoAPI.getTodos();
-
-			expect(actualTodos).toEqual([]);
-		});
-
-		it("should return todos if valid array in localstorage", () => {
-			var todos = [{
-				id: 23,
-				text: "Test all files",
-				completed: false
-			}];
-			localStorage.setItem("todos", JSON.stringify(todos));
-			var actualTodos = TodoAPI.getTodos();
-
-			expect(actualTodos).toEqual(todos);
-		});
-	});
-
 	describe("filterTodos", () => {
 		var todos = [{
 			id: 1,
