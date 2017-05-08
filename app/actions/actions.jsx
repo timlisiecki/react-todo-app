c4267ad88fa47a52c1a689f3b3763bf972238641
Tim <tim.lisiecki@gmail.com>
2017-05-08 15:53:54 -0400

    update todos location to include uid

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -29,7 +29,8 @@ export var startAddTodoItem = (text) => {
 			createdAt: moment().unix(),
 			completedAt: null
 		};
-		var todoRef = firebaseRef.child("todos").push(todo);
+		var uid = getState().auth.uid;
+		var todoRef = firebaseRef.child(`users/${uid}/todos`).push(todo);
 
 		return todoRef.then(() => {
 			dispatch(addTodoItem({
@@ -49,7 +50,8 @@ export var addTodos = (todos) => {
 
 export var startAddTodos = () => {
 	return (dispatch, getState) => {
-		var todosRef = firebaseRef.child("todos");
+		var uid = getState().auth.uid;
+		var todosRef = firebaseRef.child(`user/${uid}/todos`);
 
 		return todosRef.once("value").then((snapshot) => {
 			var todos = snapshot.val() || {};
@@ -77,7 +79,8 @@ export var updateTodoItem = (id, updates) => {
 
 export var startToggleTodoItem = (id, completed) => {
 	return (dispatch, getState) => {
-		var todoRef = firebaseRef.child(`todos/${id}`);
+		var uid = getState().auth.uid;
+		var todoRef = firebaseRef.child(`users/${uid}/todos/${id}`);
 		var updates = {
 			completed,
 			completedAt: completed ? moment().unix() : null
